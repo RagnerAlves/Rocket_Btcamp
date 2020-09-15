673387e78df517750351eca42d258d3a731af1ac
Ragner Alves <ragnerf@hotmail.com>
2020-09-15 10:27:05 -0300

    conrfig-nodemon

--- revisando/backend/src/index.js
+++ revisando/backend/src/index.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express()
 
 app.get('/projects', (request, response) => {
-    return response.json({message: 'Hello Word'})
+    return response.json({message: 'Hello GoStack'})
 })
 
-app.listen(3335)
+app.listen(3335, () => {
+    console.log('Back-end started!')
+})
