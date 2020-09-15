dd1bc62244c5003a10e659a0a0d0f379b6899b53
Ragner Alves <ragnerf@hotmail.com>
2020-09-15 10:37:05 -0300

    methods-http

--- revisando/backend/src/index.js
+++ revisando/backend/src/index.js
@@ -3,7 +3,36 @@ const express = require('express')
 const app = express()
 
 app.get('/projects', (request, response) => {
-    return response.json({message: 'Hello GoStack'})
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+    ])
+})
+
+app.post('/projects', (resquest, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 4',
+        'Projeto 5',
+    ])
+})
+
+app.put('/projects:id', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 4',
+        'Projeto 5',
+    ])
+})
+
+app.delete('/projects:id', (request, response) => {
+    return response.json([
+        'Projeto 2',
+        'Projeto 4',
+    ])
 })
 
 app.listen(3335, () => {
